d2f1b8ae4f314d63afe8f0349ac5e8ea856bcec3
ravenbarajas057 <ravenbarajas2001@gmail.com>
2025-06-18 23:40:45 +0800

    Trying a new approach on about me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,6 +17,14 @@ const About = () => {
             <p>I am passionate about creating impactful, data -driven solutions. My experience includes building applications, working with large datasets, and developing intelligent systems leveraging AI. Known for my problem-solving abilities, attention to detail, and ability to quickly learn new technologies, I excel in collaborative environments and have a proven track record of leading teams on projects.With strong communication and interpersonal skills, I am adept at guiding team members, fostering a positive work culture, and ensuring alignment with project goals to deliver inno vative solutions.</p>
           </div>
         </div>
+        <div className="about-section">
+          <div className="about-description-header">
+            <h3>An AI/ML Engineer with a strong background in Software development </h3> 
+          </div>
+          <div className="about-description">
+            <p>I am passionate about creating impactful, data -driven solutions. My experience includes building applications, working with large datasets, and developing intelligent systems leveraging AI. Known for my problem-solving abilities, attention to detail, and ability to quickly learn new technologies, I excel in collaborative environments and have a proven track record of leading teams on projects.With strong communication and interpersonal skills, I am adept at guiding team members, fostering a positive work culture, and ensuring alignment with project goals to deliver inno vative solutions.</p>
+          </div>
+        </div>
         <div className="hero-stats">
           <div className="stat-item">
             <span className="stat-number">15+</span>
